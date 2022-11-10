5bd07a9c768c8caeb4eb01b893581537dbddc1f7
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-10 11:28:00 +1030

    Add all audio buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import React, { useEffect } from 'react';
 import hee from './audio_files/hee-hee.mp3';
+import wow1 from './audio_files/wow1.mp3';
+import wow2 from './audio_files/wow2.mp3';
+import wow3 from './audio_files/wow3.mp3';
+import wow4 from './audio_files/wow4.mp3';
+import wow5 from './audio_files/wow5.mp3';
+import wow6 from './audio_files/wow6.mp3';
+import wow7 from './audio_files/wow7.mp3';
+import wow8 from './audio_files/wow8.mp3';
 
 
 function MyButton(props) {
@@ -40,8 +48,17 @@ class App extends React.Component {
         <h1>Drum Machine</h1>
         <div className="centerBox">
           <p>volume goes here</p>
-          <p>buttons go in here</p>
-          <MyButton audioSource={hee} actionKey={'q'} />
+          <div className="buttonHolder">
+            <MyButton audioSource={wow1} actionKey={'q'} />
+            <MyButton audioSource={wow2} actionKey={'w'} />
+            <MyButton audioSource={wow3} actionKey={'e'} />
+            <MyButton audioSource={wow4} actionKey={'a'} />
+            <MyButton audioSource={wow5} actionKey={'s'} />
+            <MyButton audioSource={wow6} actionKey={'d'} />
+            <MyButton audioSource={wow7} actionKey={'z'} />
+            <MyButton audioSource={wow8} actionKey={'x'} />
+            <MyButton audioSource={hee} actionKey={'c'} />
+          </div>
         </div>
       </div>
     );
