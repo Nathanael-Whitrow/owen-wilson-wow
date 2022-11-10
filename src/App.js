097b1fc7fbee43fc555c2fb3996b2e205101fe70
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-10 11:15:14 +1030

    Working audio button with keypress and move audio files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import hee from './audio_files/hee-hee.mp3';
 
 
+function MyButton(props) {
+
+  const playSound = () => new Audio(props.audioSource).play();
+
+  const detectKeyDown = (e) => {
+    if (e.key === props.actionKey) {
+      playSound();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', detectKeyDown, true);
+  })
+
+  return (
+    <button onClick={playSound}>
+      {props.actionKey}
+    </button>
+  )
+}
 
 
 class App extends React.Component {
@@ -10,22 +31,17 @@ class App extends React.Component {
     this.state = {
       someState: 'derp',
     }
+    // this.playSound = this.playSound.bind(this.playSound);
   }
 
   render() {
-
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-
-
-
     return (
       <div className="App">
         <h1>Drum Machine</h1>
         <div className="centerBox">
           <p>volume goes here</p>
           <p>buttons go in here</p>
-          <button></button>
+          <MyButton audioSource={hee} actionKey={'q'} />
         </div>
       </div>
     );
